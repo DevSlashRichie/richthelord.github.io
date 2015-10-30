e3cae8d0446b491220d6d688ee96f156cd09048d
BuildTools <unconfigured@null.spigotmc.org>
2015-10-29 18:42:11 -0600

    Typed Added

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+
+	var msg = [
+		"I like programming",
+		"My favorite hobbie is programming",
+		"I do my codes with love",
+		"Technology is life",
+		"Games = Programming", 
+		"This texts are awesome",
+		"Programation is life",
+		"Please scroll down",
+		"if(reader.isReading() == true) ^500{ Write.more() }^500",
+		"RichTheLord is the best developer",
+		"I want to work in hypixel network",
+		"Rembember, I'm RichTheLord",
+		"I have the cheapest prices",
+		"http://RichTheLord.com^100",
+		"I want a new computer",
+		"Apple are the best"
+	];
+
+	$(".txts").typed({
+            strings: msg,
+            typeSpeed: 20,
+            loop: true,
+            backDelay: 600
+	});
+});
