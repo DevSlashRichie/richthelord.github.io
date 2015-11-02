113608997ad1aa43ae9877928a49b7692765cc19
BuildTools <unconfigured@null.spigotmc.org>
2015-11-01 21:32:36 -0600

    UPDATE FINALLY

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TheSkyLords</title>
+</head>
+<body>
+	
+	<script>
+			window.location.href = "http://richthelord.com/";
+	</script>
+
+</body>
+</html>
